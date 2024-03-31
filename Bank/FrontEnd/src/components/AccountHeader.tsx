d3c05ad7f7b0928f420c7a24c222429d98db1926
Muaz Abdin <muathabd25@gmail.com>
2024-03-31 10:23:40 +0300

    color balance in FE

--- Bank/FrontEnd/src/components/AccountHeader.tsx
+++ Bank/FrontEnd/src/components/AccountHeader.tsx
@@ -14,10 +14,19 @@ function AccountHeader({
         account no. <strong>&lt;{accountNumFormatter(number)}&gt;</strong>
       </div>
       <div className="account-balance">
-        balance: <strong>₪{balance}</strong>
+        balance:{" "}
+        <strong style={{ color: `${getBalanceColor(balance)}` }}>
+          {`${balance < 0 ? `-${balance}` : balance}`}₪
+        </strong>
       </div>
     </Wrapper>
   );
 }
 
 export default AccountHeader;
+
+function getBalanceColor(balance: number) {
+  if (balance <= 0) return "red";
+  if (balance <= 300) return "orange";
+  return "green";
+}
